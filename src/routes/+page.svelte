717bd53414dcc668013648f20b2ed8d5a14621f1
lucastroncoso <lucastroncoso.seguros@gmail.com>
2023-06-19 13:33:17 -0300

    Add remote

---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="js">
+  import { useChat } from "ai/svelte";
+
+  const { messages, handleSubmit, input } = useChat({
+    api: "/chat",
+  });
+</script>
+
+<ul>
+  {#each $messages as message}
+    <li>{message.role} says... {message.content}</li>
+  {/each}
+</ul>
+
+<form on:submit|preventDefault={handleSubmit}>
+  <input bind:value={$input} />
+  <button>Send</button>
+</form>
